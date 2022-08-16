e098a3a9a5c384058e023adf9d06d1b8e78134fb
danielmoore24 <daniel.moore@officeforlifesciences.gov.uk>
2022-08-16 10:46:38 +0000

    added sex graph

--- analysis/report.R
+++ analysis/report.R
@@ -2,12 +2,19 @@ library('tidyverse')
 
 df_input<- read_csv(
     here::here("output", "input.csv"),
-    col_types = cols(patient_id = col_integer(),age = col_double())
+    col_types = cols(patient_id = col_integer(),age = col_double(), sex=col_factor())
 )
 
 plot_age <- ggplot(data=df_input, aes(df_input$age)) + geom_histogram()
 
 ggsave(
     plot= plot_age,
-    filename="descriptive.png", path=here::here("output")
+    filename="descriptive_age.png", path=here::here("output")
+)
+
+plot_sex <- ggplot(data=df_input, aes(df_input$sex)) + geom_bar()
+
+ggsave(
+    plot= plot_sex,
+    filename="descriptive_sex.png", path=here::here("output")
 )
